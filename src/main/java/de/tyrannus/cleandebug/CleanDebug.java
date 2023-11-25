af86ca9a642ccf28723f24ad87ec86ed6ea1b3da
tyrannus00 <dadawdawdadawdwa@gmail.com>
2023-11-25 20:25:32 +0100

    Updated hide distant horizons & general code improvements

--- src/main/java/de/tyrannus/cleandebug/CleanDebug.java
+++ src/main/java/de/tyrannus/cleandebug/CleanDebug.java
@@ -1,23 +1,131 @@
 package de.tyrannus.cleandebug;
 
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.world.World;
 
 import java.util.List;
 
 public class CleanDebug implements ClientModInitializer {
 
-    @Override
-    public void onInitializeClient() {
-        CleanDebugConfig.init("clean-debug", CleanDebugConfig.class);
+    public static void modifyLeftLines(List<String> lines) {
+        if (CleanDebugConfig.hideActiveRenderer) {
+            lines.removeIf(s -> s.startsWith("[Fabric] Active renderer:"));
+        }
+
+        if (CleanDebugConfig.hideDebugHints) {
+            lines.removeIf(s -> s.contains("Debug: Pie"));
+        }
+
+        if (CleanDebugConfig.hideHelpShortcut) {
+            lines.removeIf(s -> s.equals("For help: press F3 + Q"));
+            lines.removeIf(s -> s.startsWith("Debug charts:"));  // since 1.20.2
+        }
+
+        if (CleanDebugConfig.hideIris) {
+            lines.removeIf(s -> s.startsWith("[Iris]"));
+            lines.removeIf(s -> s.startsWith("[Entity Batching]"));
+        }
+
+        if (CleanDebugConfig.hideLitematica) {
+            lines.removeIf(s -> s.startsWith("§6[Litematica]§r"));
+        }
+
+        if (CleanDebugConfig.hideEntityCulling) {
+            lines.removeIf(s -> s.startsWith("[Culling]"));
+        }
+
+        if (CleanDebugConfig.hideViaFabric) {
+            lines.removeIf(s -> s.startsWith("[ViaFabric]"));
+        }
+
+        if (CleanDebugConfig.hideNoiseRouter) {
+            lines.removeIf(s -> s.startsWith("NoiseRouter"));
+        }
     }
 
-    public static int indexOfStartingWith(List<String> list, String startingWith) {
+    public static void modifyRightLines(List<String> lines) {
+        if (CleanDebugConfig.hardwareMode == CleanDebugConfig.HardwareMode.NONE) {
+            lines.removeIf(s -> s.startsWith("Java:"));
+            lines.removeIf(s -> s.startsWith("Mem:"));
+            lines.removeIf(s -> s.startsWith("Allocation rate:"));
+            lines.removeIf(s -> s.startsWith("Allocated:"));
+            lines.removeIf(s -> s.startsWith("Off-Heap:"));
+            lines.remove(0);
+            lines.removeIf(s -> s.startsWith("Direct Buffers:"));
+        }
+
+        if (CleanDebugConfig.hardwareMode != CleanDebugConfig.HardwareMode.ALL) {
+            var cpuIndex = indexOfStartingWith(lines, "CPU: ", false);
+
+            if (cpuIndex != -1) {
+                lines.subList(cpuIndex, Math.min(cpuIndex + 6, lines.size())).clear();
+            }
+        }
+
+        if (CleanDebugConfig.hideTags) {
+            lines.removeIf(s -> s.startsWith("#"));
+        }
+
+        if (CleanDebugConfig.hideSodium) {
+            var sodiumIndex = indexOfStartingWith(lines, "§aSodium Renderer", false);
+
+            if (sodiumIndex != -1) {
+                lines.subList(sodiumIndex, Math.min(sodiumIndex + 6, lines.size())).clear();
+            }
+        }
+
+        if (CleanDebugConfig.hideIris) {
+            lines.removeIf(s -> s.startsWith("[Iris]"));
+        }
+
+        if (CleanDebugConfig.hideDistantHorizons) {
+            var lodModIndexStart = indexOfStartingWith(lines, "Distant Horizons version: ", false);
+            var lodModIndexEnd = indexOfStartingWith(lines, "ON_LOADED:", true);
+
+            if (lodModIndexStart != -1 && lodModIndexEnd != -1) {
+                lines.subList(lodModIndexStart - 1, lodModIndexEnd + 1).clear();    // Adds empty line before
+            }
+        }
+
+        if (CleanDebugConfig.hideModernFix) {
+            var modernFixIndex = indexOfStartingWith(lines, "ModernFix", false);
+
+            if (modernFixIndex != -1) {
+                lines.subList(modernFixIndex, Math.min(modernFixIndex + 2, lines.size())).clear();
+            }
+        }
+
+        while (!lines.isEmpty() && lines.get(0).isEmpty()) {
+            lines.remove(0);
+        }
+
+    }
+
+    public static HitResult.Type getFluidHitResultType(HitResult result, World world) {
+        if (CleanDebugConfig.shyFluids && result instanceof BlockHitResult blockHitResult && world.getFluidState(blockHitResult.getBlockPos()).isEmpty()) {
+            return HitResult.Type.MISS;
+        }
+
+        return result.getType();
+    }
+
+    private static int indexOfStartingWith(List<String> list, String startingWith, boolean trim) {
         for (var i = 0; i < list.size(); i++) {
-            if (list.get(i).startsWith(startingWith)) {
+            var string = list.get(i);
+
+            if ((trim ? string.trim() : string).startsWith(startingWith)) {
                 return i;
             }
         }
 
         return -1;
+    }
+
+    @Override
+    public void onInitializeClient() {
+        MidnightConfig.init("clean-debug", CleanDebugConfig.class);
     }
 }
--- src/main/java/de/tyrannus/cleandebug/mixins/DebugHudMixin.java
+++ src/main/java/de/tyrannus/cleandebug/mixins/DebugHudMixin.java
@@ -1,18 +1,15 @@
 package de.tyrannus.cleandebug.mixins;
 
 import de.tyrannus.cleandebug.CleanDebug;
-import de.tyrannus.cleandebug.CleanDebugConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.DebugHud;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -20,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import java.util.List;
 
 @Mixin(value = DebugHud.class, priority = 900)
-public class DebugHudMixin {
+public abstract class DebugHudMixin {
 
     @Shadow
     @Final
@@ -30,116 +27,30 @@ public class DebugHudMixin {
             method = "drawLeftText",
             at = @At(
                     value = "INVOKE",
-                    target = "Ljava/util/List;add(Ljava/lang/Object;)Z",
-                    shift = At.Shift.AFTER,
-                    ordinal = 2
+                    target = "Lnet/minecraft/client/gui/hud/DebugHud;drawText(Lnet/minecraft/client/gui/DrawContext;Ljava/util/List;Z)V",
+                    shift = At.Shift.BEFORE
             ),
             locals = LocalCapture.CAPTURE_FAILHARD
     )
-    private void onRenderLeftText(DrawContext context, CallbackInfo ci, List<String> lines) {
-        if (CleanDebugConfig.hideActiveRenderer) {
-            lines.removeIf(s -> s.startsWith("[Fabric] Active renderer:"));
-        }
-
-        if (CleanDebugConfig.hideDebugHints) {
-            lines.removeIf(s -> s.contains("Debug: Pie"));
-        }
-
-        if (CleanDebugConfig.hideHelpShortcut) {
-            lines.removeIf(s -> s.equals("For help: press F3 + Q"));
-            lines.removeIf(s -> s.startsWith("Debug charts:"));  // since 1.20.2
-        }
-
-        if (CleanDebugConfig.hideIris) {
-            lines.removeIf(s -> s.startsWith("[Iris]"));
-            lines.removeIf(s -> s.startsWith("[Entity Batching]"));
-        }
-
-        if (CleanDebugConfig.hideLitematica) {
-            lines.removeIf(s -> s.startsWith("§6[Litematica]§r"));
-        }
-
-        if (CleanDebugConfig.hideEntityCulling) {
-            lines.removeIf(s -> s.startsWith("[Culling]"));
-        }
-
-        if (CleanDebugConfig.hideViaFabric) {
-            lines.removeIf(s -> s.startsWith("[ViaFabric]"));
-        }
-
-        if (CleanDebugConfig.hideNoiseRouter) {
-            lines.removeIf(s -> s.startsWith("NoiseRouter"));
-        }
+    private void modifyDrawLeftText(DrawContext ignored, CallbackInfo ci, List<String> lines) {
+        CleanDebug.modifyLeftLines(lines);
     }
 
-    @ModifyVariable(method = "drawRightText", at = @At(value = "STORE"))
-    private List<String> modifyLines(List<String> lines) {
-        // using old switch without break on purpose
-        switch (CleanDebugConfig.hardwareMode) {
-            case NONE: {
-                lines.removeIf(s -> s.startsWith("Java:"));
-                lines.removeIf(s -> s.startsWith("Mem:"));
-                lines.removeIf(s -> s.startsWith("Allocation rate:"));
-                lines.removeIf(s -> s.startsWith("Allocated:"));
-                lines.removeIf(s -> s.startsWith("Off-Heap:"));
-                lines.remove(0);
-                lines.removeIf(s -> s.startsWith("Direct Buffers:"));
-            }
-            case REDUCED: {
-                var cpuIndex = -1;
-
-                for (var i = 0; i < lines.size(); i++) {
-                    if (lines.get(i).startsWith("CPU: ")) {
-                        cpuIndex = i;
-                        break;
-                    }
-                }
-
-                if (cpuIndex != -1) {
-                    lines.subList(cpuIndex, Math.min(cpuIndex + 6, lines.size())).clear();
-                }
-            }
-        }
-
-        if (CleanDebugConfig.hideTags) {
-            lines.removeIf(s -> s.startsWith("#"));
-        }
-
-        if (CleanDebugConfig.hideSodium) {
-            var sodiumIndex = CleanDebug.indexOfStartingWith(lines, "§aSodium Renderer");
-
-            if (sodiumIndex != -1) {
-                lines.subList(sodiumIndex, Math.min(sodiumIndex + 6, lines.size())).clear();
-            }
-        }
-
-        if (CleanDebugConfig.hideIris) {
-            lines.removeIf(s -> s.startsWith("[Iris]"));
-        }
-
-        if (CleanDebugConfig.hideDistantHorizons) {
-            lines.removeIf(s -> s.startsWith("Distant Horizons"));
-        }
-
-        if (CleanDebugConfig.hideModernFix) {
-            var modernFixIndex = CleanDebug.indexOfStartingWith(lines, "ModernFix");
-
-            if (modernFixIndex != -1) {
-                lines.subList(modernFixIndex, Math.min(modernFixIndex + 2, lines.size())).clear();
-            }
-        }
-
-        return lines;
+    @Inject(
+            method = "drawRightText",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/hud/DebugHud;drawText(Lnet/minecraft/client/gui/DrawContext;Ljava/util/List;Z)V",
+                    shift = At.Shift.BEFORE
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
+    private void modifyDrawRightText(DrawContext ignored, CallbackInfo ci, List<String> lines) {
+        CleanDebug.modifyRightLines(lines);
     }
 
     @Redirect(method = "getRightText", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/hit/HitResult;getType()Lnet/minecraft/util/hit/HitResult$Type;", ordinal = 1))
-    protected final HitResult.Type fluidVisibility(HitResult result) {
-        if (CleanDebugConfig.shyFluids && result instanceof BlockHitResult blockHitResult) {
-            if (client.world != null && client.world.getFluidState(blockHitResult.getBlockPos()).isEmpty()) {
-                return HitResult.Type.MISS;
-            }
-        }
-
-        return result.getType();
+    protected final HitResult.Type changeFluidHitType(HitResult result) {
+        return CleanDebug.getFluidHitResultType(result, client.world);
     }
 }
